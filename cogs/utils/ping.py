9a491c21664aa50dbb5486e7c2f1f66bbf86c22c
ChiliPy <kaspian.nunno@icloud.com>
2021-02-20 11:40:33 -0600

    comments ™️

--- cogs/utils/ping.py
+++ cogs/utils/ping.py
@@ -3,20 +3,34 @@ from discord.ext import commands
 
 import time
 
-
+# creating our new cog
 class latencies(commands.Cog):
+
+    """
+    initializing the class
+    defining bot
+    """
+
     def __init__(self, bot):
         self.bot = bot
 
 
     @commands.command(help="get the bots different pings")
     async def ping(self, ctx):
+        # starting our ping counter
         start_time = time.time()
         msg = await ctx.send("Testing Ping...")
+        # ending our ping counter
         end_time = time.time()
+
+        """
+        editing our original message to display the bots different pings
+        """
 
         await msg.edit(content=f"BOT: {round(self.bot.latency * 1000)}ms\nAPI: {round((end_time - start_time) * 1000)}ms")
 
 
+
 def setup(bot):
+    # setting up our cog
     bot.add_cog(latencies(bot))
